2a16aabd84e667f7e009ab7b6427e5f57252e828
Nii Botchway <ishmoto4u@gmail.com>
2020-09-01 14:45:57 +0000

    Add project files.

---
+++ BackEnd/Data/Speaker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BackEnd.Data
+{
+    public class Speaker : ConferenceDTO.Speaker
+    {
+        public virtual ICollection<SessionSpeaker> SessionSpeakers { get; set; } = new List<SessionSpeaker>();
+    }
+}
