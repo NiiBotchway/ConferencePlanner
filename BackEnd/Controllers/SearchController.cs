ca60e470cf75c103cdd14e29edb556a83de630d1
Nii Botchway <ishmoto4u@gmail.com>
2020-09-02 10:40:06 +0000

    Added Search Functionality to FrontEnd Project

---
+++ BackEnd/Controllers/SearchController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BackEnd.Data;
+using BackEnd.Infrastructure;
+using ConferenceDTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SearchController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SearchResult>>> Search(SearchTerm term)
+        {
+            var query = term.Query;
+            var sessionResults = (await _context.Sessions.Include(s => s.Track)
+                                                .Include(s => s.SessionSpeakers)
+                                                    .ThenInclude(ss => ss.Speaker)
+                                                 .ToListAsync())
+                                                .Where(s =>
+                                                    s.Title.Contains(query, System.StringComparison.InvariantCultureIgnoreCase) ||
+                                                    s.Track.Name.Contains(query, System.StringComparison.InvariantCultureIgnoreCase)
+                                                )
+                                                .ToList();
+
+            var speakerResults = (await _context.Speakers.Include(s => s.SessionSpeakers)
+                                                    .ThenInclude(ss => ss.Session)
+                                                .ToListAsync())
+                                                .Where(s =>
+                                                    (s.Name?.Contains(query, System.StringComparison.InvariantCultureIgnoreCase) ?? false) ||
+                                                    (s.Bio?.Contains(query, System.StringComparison.InvariantCultureIgnoreCase) ?? false) ||
+                                                    (s.WebSite?.Contains(query, System.StringComparison.InvariantCultureIgnoreCase) ?? false)
+                                                )
+                                                .ToList();
+
+            var results = sessionResults.Select(s => new SearchResult
+            {
+                Type = SearchResultType.Session,
+                Session = s.MapSessionResponse()
+            })
+            .Concat(speakerResults.Select(s => new SearchResult
+            {
+                Type = SearchResultType.Speaker,
+                Speaker = s.MapSpeakerResponse()
+            }));
+
+            return results.ToList();
+        }
+    }
+}
